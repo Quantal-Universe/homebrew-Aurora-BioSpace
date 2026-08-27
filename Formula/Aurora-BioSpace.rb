986a8ca2384412a858affb38d408a56b9538d889
Quantal Labs <main@quantal-labs.com>
2026-08-27 18:49:37 +0400

    Update Aurora-BioSpace.rb

--- Formula/Aurora-BioSpace.rb
+++ Formula/Aurora-BioSpace.rb
@@ -1,7 +1,7 @@
 class AuroraBiospace < Formula
   desc "Launcher for the Aurora Bioscience Dashboard by Quantal-Labs, founded by Codemaster-AR."
   homepage "https://github.com/Quantal-Labs/Aurora-BioSpace"
-  url "https://github.com/Quantal-Labs/Aurora-BioSpace/archive/refs/tags/v8.0.0.tar.gz"
+  url "https://github.com/Quantal-Universe/Aurora-BioSpace/archive/refs/tags/v8.0.0.tar.gz"
   sha256 "c9d7222fadc31921dd82ccbab6e88f75818f06229894664fd5395e59258ccafb"
   version "8.0.0"
 
